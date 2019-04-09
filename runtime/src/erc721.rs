66d97d0ac117192a11a19457f8d93891f43ad891
nczhu <nicolezhuis@gmail.com>
2019-04-09 17:15:20 +0200

    Add: Enclosed erc test setup into its own mod

--- runtime/src/erc721.rs
+++ runtime/src/erc721.rs
@@ -328,40 +328,50 @@ impl<T: Trait> Module<T> {
 // TESTING
 
 #[cfg(test)]
-use super::*;
+mod tests {
+    use super::*;
 
-// Import necessary types
-use primitives::{H256, Blake2Hasher}; //called substrate_primitives as primitives
+    use primitives::{H256, Blake2Hasher}; //called substrate_primitives as primitives
+    use runtime_primitives::{
+        traits::{IdentityLookup, BlakeTwo256}, // Test wrapper for this specific type/ looks up the identity; returns Result
+        testing::{Digest, DigestItem, Header}
+    };
+    use support::{impl_outer_origin};
 
-use runtime_primitives::{
-    traits::{IdentityLookup}, // Test wrapper for this specific type/ looks up the identity; returns Result
-    testing::{Digest, DigestItem, Header}
-};
+    // impl outer origin
+    impl_outer_origin! {
+        pub enum Origin for ERCTest {}
+    }
 
-// 2. Set up mock runtime
-pub struct Runtime;
+    // 2. Set up mock runtime
+    pub struct ERCTest; // can call thsi anything Runtime or Test...
 
-// #[derive(Clone, PartialEq, Eq, Debug)]
-impl system::Trait for Runtime {
-    type Origin = Origin;  // these types are declared in the module traits, so they must be ste
-    type Index = u64;       //hack it to just be a u64 int (later: double check the actual type?)
-    type BlockNumber = u64;
-    type Hash = H256;
-    type Hashing = BlakeTwo256;
-    type Digest = Digest;
-    type AccountId = u64;
-    type Lookup = IdentityLookup<Self::AccountId>;
-    type Header = Header;
-    type Event = ();
-    type Log = DigestItem;
-}
+    // #[derive(Clone, PartialEq, Eq, Debug)]
+    impl system::Trait for ERCTest {
+        type Origin = Origin;  // these types are declared in the module traits, so they must be ste
+        type Index = u64;       //hack it to just be a u64 int (later: double check the actual type?)
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Hashing = BlakeTwo256; //from runtime_primitives::traits::blaketwo256
+        type Digest = Digest;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = ();
+        type Log = DigestItem;
+    }
 
-impl balances::Trait for Runtime {
-    type Balance = u64; //hack it to be a u64 figure
-    type OnFreeBalanceZero = (); //dont give it a variable? unless you have an implemntation e.g. freebalance in staking module
-    type OnNewAccount = ();
-    type TransactionPayment = ();
-    type TransferPayment = ();
-    type DustRemoval = ();
+    impl balances::Trait for ERCTest {
+        type Balance = u64; //hack it to be a u64 figure
+        type OnFreeBalanceZero = (); //overrides. () is to use the default. 
+        type OnNewAccount = ();
+        type TransactionPayment = ();
+        type TransferPayment = ();
+        type DustRemoval = ();
+    }
+
+    // impl the types for this particular trait!
+    // impl Trait for Runtime
+    
 }
 
