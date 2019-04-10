18c3a796767b77d93ca326334f1e4aa09ea96742
nczhu <nicolezhuis@gmail.com>
2019-04-10 13:06:25 +0200

    Add fn create_debt_request

--- runtime/src/collateral.rs
+++ runtime/src/collateral.rs
@@ -3,33 +3,40 @@
 	- Moment trait
 */
 
+use crate::erc721;		// our ERC 721 implementation
+
 use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
 use system::ensure_signed;
 
-// import currency trait, to get access to "ensure can withdraw", everything for balance. 
-use support::traits::{Currency}; // Other avail traits lockablecurrency, onfreebalancezero, etc.
+// import currency trait, to get access to "ensure_can_withdraw", everything for balance. 
+// use support::traits::{Currency}; // Other avail traits lockablecurrency, onfreebalancezero, etc.
 
+// Currency trait, needs this internal type (in order to input things into fn signatures inputs: e.g. #[compact] value: BalanceOf<T>
+// type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
 
-pub trait Trait: system::Trait {
+// This module's traits
+pub trait Trait: balances::Trait { // need to add timstam and things here?
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
+// Status of the debtors request
 enum RequestStatus {
 	Draft,
 	Collateralized,
 }
 
 // Asset owners can create a DebtRequest to ask for a traunche of Balance
-pub struct DebtRequest<Hash, AccountId, Currency, Moment> {   //Needs the blake2 Hash trait
+pub struct DebtRequest<Hash, AccountId, Balance, Moment> {   //Needs the blake2 Hash trait
 	id: Hash,								// DebtRequestId
 	requestor: AccountId,		// Account that will go in debt
 	beneficiary: AccountId,	// Recipient of Balance
-	amount: Currency,				// Amount of loan
+	amount: Balance,				// Amount of loan
 	expiry: Moment,					// Duration of debtRequest
 	collateralized: bool,		// Defaults to false, true upon collaterlization
 	status: RequestStatus,	// status of this request
 }
 
+// Status of the collateralized debt
 enum OrderStatus {
 	Expired,		// loan is never filled, expired
 	Open, 			// looking for issuance
@@ -44,30 +51,50 @@ pub struct DebtOrder<Hash, AccountId, Moment> {
 	requestId: Hash,				// corresponding DebtRequestId
 	status: OrderStatus,		// status of this order
 	creditor: AccountId,
-	
 	// Input by debtor
 	expiry: Moment,					// Due date for all payment
 	// TODO collateral of tokens...  // a fixed length array of tokens collateralized in system escrow
 }
 
-
 decl_storage! {
-	trait Store for Module<T: Trait> as Collateral {
+	trait Store for Module<T: Trait> as CollateralStorage {
 		
 		DebtRequests get(get_debt_order): map T::Hash => DebtRequest<T::Hash>; //DebtRequest ID to the RequestItself
 
 		// Escrow get(escrow): //hash of tokenID under management
-
 	}
 }
 
+// writes functions, make sure to declares all traits where using here in: 
+// pub trait Trait
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		
 		fn deposit_event<T>() = default;
 
-		// DEBTOR:
-		// pub fn create_debt_request
+		// DEBTOR FUNCTIONS: 
+		pub fn create_debt_request(origin, amount: T::Balance, beneficiary: T::AccountId, expiry: u64) { //TODO, change expiry
+			use RequestStatus::Draft;		// have to import to this the enum
+
+			let sender = ensure_signed(origin)?;		//macro, returns sender address
+
+			// TODO do some checks here
+			// let id = (<system::Module<T>>::random_seed(), &sender, nonce).using_encoded(<T as system::Trait>::Hashing::hash);
+			let now = <timestamp::Module<T>>::get();
+			let id = (<system::Module<T>>::random_seed(), &sender, now).using_encoded(<T as system::Trait>::Hashing::hash);
+
+			// TODO make sure debtrequest doesn't exist already, in case they try to overwrite debt..
+
+			let requestor = beneficiary;
+			let collateralized = false;
+			let status = Draft;
+
+			let new_debt_request = DebtRequest { id, requestor, beneficiary, amount, expiry, collateralized, status};
+
+			// make a hash for this dbt request
+
+
+		}
 
 		// pub fn collateralize_debt_request (stake n tokens?)
 
@@ -76,14 +103,11 @@ decl_module! {
 		// LOANER:
 		// pub fn fill_debt_order
 		
-
 		// SYSTEM:     		// Removes the need for a trusted contract, etc. system maintains
 		// fn return_collateral
 		// fn seize_collateral
 
-
 		// on_intialize().. // 
-
 
 	}
 }
